e3bb955e75132d389a46d4f312e4921e7969bed2
StuartCharters <charters.stuart@gmail.com>
2018-05-30 18:08:01 +0100

    can add player

--- game.rb
+++ game.rb
@@ -4,6 +4,15 @@ class Game
 
   def initialize(name)
     @name = name
+    @players = []
+  end
+
+  def number_of_players()
+    @players.length()
+  end
+
+  def add_player(player)
+    @players.push(player)
   end
 
 end
--- specs/game_spec.rb
+++ specs/game_spec.rb
@@ -1,16 +1,23 @@
 require("minitest/autorun")
 require_relative("../game.rb")
 require_relative("../hiddenword.rb")
+require_relative("../player.rb")
 
 class GameTest < MiniTest::Test
 
   def setup
     @game1 = Game.new("test1")
     @word1 = HiddenWord.new("hello")
+    @player1 = Player.new("Stuart")
   end
 
   def test_game_has_name
     assert_equal("test1", @game1.name())
+  end
+
+  def test_add_a_player
+    @game1.add_player(@player1)
+    assert_equal(1,@game1.number_of_players())
   end
 
 end
